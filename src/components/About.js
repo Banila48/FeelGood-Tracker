7317df8b282d003225f430dcb1123dd93499a8a4
Banila <royng34@gmail.com>
2021-07-20 20:57:16 +0800

    Initial

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div>
+      <h4>First React Project</h4>
+      <Link to='/'>Home</Link>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p><i>A Little Progress Each Day Adds Up To Big Results</i></p>
+      <Link to='/about'>About</Link>
+    </footer>
+  )
+}
+
+export default Footer
